94cd0b440d3312bacaad69f7fd08eae001a9c982
Pat Monette <patrick@ontarioseo.ca>
2022-04-01 14:10:15 +0000

    blog styling update

--- loop-templates/content.php
+++ loop-templates/content.php
@@ -12,11 +12,11 @@ defined( 'ABSPATH' ) || exit;
 <article class="card mb-3" id="post-<?php the_ID(); ?>">
 
 	<div class="row align-items-center">
-			<div class="col-5">
-				<?php echo get_the_post_thumbnail( $post->ID, 'blogthumb', array('class' => 'w-100') ); ?>
+			<div class="col-md-5">
+				<?php echo get_the_post_thumbnail( $post->ID, 'blogthumb', array('class' => 'w-100 mb-0 mb-md-3') ); ?>
 			</div>
 		
-			<div class="col-7">
+			<div class="col-md-7 p-3 p-md-0">
 				<?php if ( 'post' === get_post_type() ) : ?>
 		
 					<div class="entry-meta">
