3f7c096eaa1939196d430e3d6acbc8aa8ba4fc2a
Pat Monette <patrick@ontarioseo.ca>
2022-04-01 14:14:00 +0000

    mobile blog update

--- loop-templates/content.php
+++ loop-templates/content.php
@@ -16,17 +16,19 @@ defined( 'ABSPATH' ) || exit;
 				<?php echo get_the_post_thumbnail( $post->ID, 'blogthumb', array('class' => 'w-100 mb-0 mb-md-3') ); ?>
 			</div>
 		
-			<div class="col-md-7 p-3 p-md-0">
-				<?php if ( 'post' === get_post_type() ) : ?>
-		
-					<div class="entry-meta">
-		
-						<?php the_date('F j, Y'); ?>
-		
-					</div><!-- .entry-meta -->
-		
-				<?php endif; ?>
-				<a href="<?php the_permalink(); ?>" class="stretched-link"><h2><?php the_title(); ?></h2></a>
+			<div class="col-md-7">
+				<div class="p-3 p-md-0">
+					<?php if ( 'post' === get_post_type() ) : ?>
+			
+						<div class="entry-meta">
+			
+							<?php the_date('F j, Y'); ?>
+			
+						</div><!-- .entry-meta -->
+			
+					<?php endif; ?>
+					<a href="<?php the_permalink(); ?>" class="stretched-link"><h2><?php the_title(); ?></h2></a>
+				</div>
 			</div>
 	</div>
 
